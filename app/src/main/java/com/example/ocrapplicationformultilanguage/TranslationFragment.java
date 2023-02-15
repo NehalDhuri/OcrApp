0d1c63d408bb796dc9ba9366dbc7eff62f17d9d5
Nehal Dhuri <nehaldhuri221@gmail.com>
2023-02-16 00:06:24 +0530

    Added translation api. It working good.

--- app/src/main/java/com/example/ocrapplicationformultilanguage/TranslationFragment.java
+++ app/src/main/java/com/example/ocrapplicationformultilanguage/TranslationFragment.java
@@ -2,63 +2,104 @@ package com.example.ocrapplicationformultilanguage;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TranslationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
 public class TranslationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private TextView textView;
+    private Button button;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public TranslationFragment() {
-        // Required empty public constructor
-    }
+    OkHttpClient client;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Translation.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TranslationFragment newInstance(String param1, String param2) {
-        TranslationFragment fragment = new TranslationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    String postURL = "https://google-translate1.p.rapidapi.com/language/translate/v2";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_translation, container, false);
+        View view = inflater.inflate(R.layout.fragment_translation, container, false);
+
+//        super.onCreate(savedInstanceState);
+
+        client = new OkHttpClient();
+
+        textView = view.findViewById(R.id.demo);
+        button = view.findViewById(R.id.button);
+
+
+        button.setOnClickListener(view1 -> {
+            post();
+        });
+        return view;
+    }
+
+    public void post(){
+        RequestBody body = new FormBody.Builder()
+                .add("q", "Hello, world!")
+                .add("target", "es")
+                .add("source", "en")
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://google-translate1.p.rapidapi.com/language/translate/v2")
+                .post(body)
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .addHeader("Accept-Encoding", "application/gzip")
+                .addHeader("X-RapidAPI-Key", "1cbb6a14d5msh02d6df95b649332p165bb5jsn11cd76766d0f")
+                .addHeader("X-RapidAPI-Host", "google-translate1.p.rapidapi.com")
+                .build();
+
+//        Response response = client.newCall(request).execute();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+//                textView.setText("fail");
+
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+
+                try {
+                    String resp = Objects.requireNonNull(response.body()).string();
+
+                    JSONObject jsonObject = new JSONObject(resp);
+                    JSONObject data = jsonObject.getJSONObject("data");
+                    JSONArray translations = data.getJSONArray("translations");
+                    JSONObject translatedText = translations.getJSONObject(0);
+                    String result = translatedText.getString("translatedText");
+                    textView.setText(result);
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
