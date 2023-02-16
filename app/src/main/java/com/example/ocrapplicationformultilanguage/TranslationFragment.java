82d745a87beffe0ce1ebfe3706de27b2ac9c34fa
Nehal Dhuri <nehaldhuri221@gmail.com>
2023-02-16 16:29:38 +0530

    Translation is done.

--- app/src/main/java/com/example/ocrapplicationformultilanguage/TranslationFragment.java
+++ app/src/main/java/com/example/ocrapplicationformultilanguage/TranslationFragment.java
@@ -8,12 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.chinese.ChineseTextRecognizerOptions;
+import com.google.mlkit.vision.text.devanagari.DevanagariTextRecognizerOptions;
+import com.google.mlkit.vision.text.japanese.JapaneseTextRecognizerOptions;
+import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,13 +41,13 @@ import okhttp3.Response;
 
 public class TranslationFragment extends Fragment {
 
-    private TextView textView;
     private Button translateBtn;
     private AutoCompleteTextView autoCompleteTextViewFrom;
     private AutoCompleteTextView autoCompleteTextViewTo;
     private TextInputEditText sourceEditText;
     private TextInputEditText translatedEditText;
 
+    private String sourceLang, targetLang;
 
 
     OkHttpClient client;
@@ -58,8 +66,7 @@ public class TranslationFragment extends Fragment {
         autoCompleteTextViewTo = view.findViewById(R.id.autoCompleteTextViewTo);
 
         // data to inflate the drop-down items
-        String[] langauge_dictionary = new String[]{"Afrikaans", "Arabic", "Danish","English", "German","Hindi","Marathi"};
-//        String[] langauge_dictionary = new String[]{"Afrikaans", "Arabic", "Danish","English", "German","Hindi","Marathi"};
+        String[] langauge_dictionary = new String[]{"Afrikaans", "Arabic", "Danish", "English", "German", "Hindi", "Marathi"};
 
         // create an array adapter and pass the required parameter
         // in our case pass the context, drop down layout , and array.
@@ -67,26 +74,102 @@ public class TranslationFragment extends Fragment {
         autoCompleteTextViewFrom.setAdapter(adapter);
         autoCompleteTextViewTo.setAdapter(adapter);
 
-//        super.onCreate(savedInstanceState);
+        autoCompleteTextViewFrom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                sourceLang = sourceLanguage(langauge_dictionary);
+            }
+        });
+
+        autoCompleteTextViewTo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                targetLang = targetLanguage(langauge_dictionary);
+            }
+        });
 
         client = new OkHttpClient();
 
         sourceEditText = view.findViewById(R.id.sourceEditText);
         translatedEditText = view.findViewById(R.id.translatedEditText);
-
         translateBtn = view.findViewById(R.id.translateBtn);
 
         translateBtn.setOnClickListener(view1 -> {
-            post();
+            post(sourceLang, targetLang);
+
+
         });
         return view;
     }
 
-    public void post(){
+
+    public String sourceLanguage(String[] langauge_dictionary) {
+        String tempSourceLang = " ";
+        if (autoCompleteTextViewFrom.getText().toString().equals(langauge_dictionary[0])) {
+
+            // for Afrikaans
+            tempSourceLang = "af";
+        } else if (autoCompleteTextViewFrom.getText().toString().equals(langauge_dictionary[1])) {
+            // for Arabic
+
+            tempSourceLang = "ar";
+        } else if (autoCompleteTextViewFrom.getText().toString().equals(langauge_dictionary[2])) {
+            // for Danish
+            tempSourceLang = "da";
+        } else if (autoCompleteTextViewFrom.getText().toString().equals(langauge_dictionary[3])) {
+            // for English
+            tempSourceLang = "en";
+        } else if (autoCompleteTextViewFrom.getText().toString().equals(langauge_dictionary[4])) {
+            //German
+            tempSourceLang = "de";
+        } else if (autoCompleteTextViewFrom.getText().toString().equals(langauge_dictionary[5])) {
+            //Hindi
+            tempSourceLang = "hi";
+        } else if (autoCompleteTextViewFrom.getText().toString().equals(langauge_dictionary[6])) {
+            //Marathi
+            tempSourceLang = "mr";
+        } else {
+            Toast.makeText(getContext(), "Please select your language!", Toast.LENGTH_SHORT).show();
+        }
+        return tempSourceLang;
+    }
+
+    public String targetLanguage(String[] langauge_dictionary) {
+        String tempTargetLang = "";
+        if (autoCompleteTextViewTo.getText().toString().equals(langauge_dictionary[0])) {
+
+            // for Afrikaans
+            tempTargetLang = "af";
+        } else if (autoCompleteTextViewTo.getText().toString().equals(langauge_dictionary[1])) {
+            // for Arabic
+
+            tempTargetLang = "ar";
+        } else if (autoCompleteTextViewTo.getText().toString().equals(langauge_dictionary[2])) {
+            // for Danish
+            tempTargetLang = "da";
+        } else if (autoCompleteTextViewTo.getText().toString().equals(langauge_dictionary[3])) {
+            // for English
+            tempTargetLang = "en";
+        } else if (autoCompleteTextViewTo.getText().toString().equals(langauge_dictionary[4])) {
+            //German
+            tempTargetLang = "de";
+        } else if (autoCompleteTextViewTo.getText().toString().equals(langauge_dictionary[5])) {
+            //Hindi
+            tempTargetLang = "hi";
+        } else if (autoCompleteTextViewTo.getText().toString().equals(langauge_dictionary[6])) {
+            //Marathi
+            tempTargetLang = "mr";
+        } else {
+            Toast.makeText(getContext(), "Please select your language!", Toast.LENGTH_SHORT).show();
+        }
+        return tempTargetLang;
+    }
+
+    public void post(String sourceLang, String targetLang) {
         RequestBody body = new FormBody.Builder()
-                .add("q", "Hello, world!")
-                .add("target", "es")
-                .add("source", "en")
+                .add("q", Objects.requireNonNull(sourceEditText.getText()).toString())
+                .add("target", targetLang)
+                .add("source", sourceLang)
                 .build();
 
         Request request = new Request.Builder()
@@ -98,31 +181,36 @@ public class TranslationFragment extends Fragment {
                 .addHeader("X-RapidAPI-Host", "google-translate1.p.rapidapi.com")
                 .build();
 
-//        Response response = client.newCall(request).execute();
 
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-//                textView.setText("fail");
 
+                Toast.makeText(getContext(), "No data", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                requireActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            String resp = Objects.requireNonNull(response.body()).string();
 
-                try {
-                    String resp = Objects.requireNonNull(response.body()).string();
+                            JSONObject jsonObject = new JSONObject(resp);
+                            JSONObject data = jsonObject.getJSONObject("data");
+                            JSONArray translations = data.getJSONArray("translations");
+                            JSONObject translatedText = translations.getJSONObject(0);
+                            String result = translatedText.getString("translatedText");
+                            translatedEditText.setText(result);
+                        } catch (IOException | JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getContext(), "No data", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
-                    JSONObject jsonObject = new JSONObject(resp);
-                    JSONObject data = jsonObject.getJSONObject("data");
-                    JSONArray translations = data.getJSONArray("translations");
-                    JSONObject translatedText = translations.getJSONObject(0);
-                    String result = translatedText.getString("translatedText");
-                    textView.setText(result);
-                } catch (IOException | JSONException e) {
-                    e.printStackTrace();
-                }
 
             }
         });
