9ad1f42f7c367b8c1dd4b7ee9d65446e87a33194
Nehal Dhuri <nehaldhuri221@gmail.com>
2023-02-22 22:57:42 +0530

    Revert "File name changed."
    This reverts commit b266b04827a16f595fcf24ee1a87c850531b9c2c.

---
+++ app/src/main/java/com/example/ocrapplicationformultilanguage/MainActivity2.java
@@ -0,0 +1,60 @@
+package com.example.ocrapplicationformultilanguage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+//    ActivityMainBinding binding;
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    NotesFragment notesFragment = new NotesFragment();
+    TranslationFragment translationFragment = new TranslationFragment();
+
+    final int home = R.id.home;
+    final int notes = R.id.notes;
+    final int translator = R.id.translator;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_main2);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        replaceFragment(homeFragment);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case home:
+                    replaceFragment(homeFragment);
+                    return true;
+                case notes:
+                    replaceFragment(notesFragment);
+                    return true;
+                case translator:
+                    replaceFragment(translationFragment);
+                    return true;
+            }
+            return false;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+}
